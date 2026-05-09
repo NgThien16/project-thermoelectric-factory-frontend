3c4fa213a60a3c14742be5f99f818e68df5f007d
tiger2131231 <duongvuthai2018@gmail.com>
2026-05-09 13:50:31 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Layout/Sidebar';
 import TopNavbar from './components/Layout/TopNavbar';
-import HRManagement from './pages/HR/HRManagement';
-import EquipmentManagement from './pages/Operation/EquipmentManagement';
-import MaintenanceManagement from './pages/Maintenance/MaintenanceManagement';
 import WarehouseManagement from './pages/Warehouse/WarehouseManagement';
 import ToolManagement from './pages/Warehouse/ToolManagement';
 import { ToastContainer } from 'react-toastify';
@@ -23,9 +20,6 @@ function App() {
           <div className="content-wrapper flex-grow-1">
             <Routes>
               <Route path="/" element={<div className="p-4"><h3>Dashboard</h3><p>Chào mừng đến với hệ thống quản lý nhà máy nhiệt điện.</p></div>} />
-              <Route path="/nhan-su" element={<HRManagement />} />
-              <Route path="/van-hanh" element={<EquipmentManagement />} />
-              <Route path="/sua-chua" element={<MaintenanceManagement />} />
               <Route path="/kho" element={<WarehouseManagement />} />
               <Route path="/ccdc" element={<ToolManagement />} />
             </Routes>
